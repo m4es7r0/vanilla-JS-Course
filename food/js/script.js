0f26771c694497252a9328a179c6aa443c6531e0
m4e7r0 <dodogof02@gmail.com>
2022-07-28 15:51:46 +0300

    UPD: added animation

--- food/js/script.js
+++ food/js/script.js
@@ -7,14 +7,15 @@ window.addEventListener('DOMContentLoaded', () => {
     function hideTabContent() {
         tabsContent.forEach(item => {
             item.classList.add('hide');
-            item.classList.remove('show');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(tab => tab.classList.remove('tabheader__item_active'));
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show');
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
