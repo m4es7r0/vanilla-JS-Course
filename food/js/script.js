96e259ff77c54e786b6e267f63e87195577cec3c
m4e7r0 <dodogof02@gmail.com>
2022-07-28 13:10:32 +0300

    tabs

---
+++ food/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tebsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+
+        tabs.forEach(tab => tab.classList.remove('tabheader__item_active'));
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tebsParent.addEventListener('click', (e) => {
+        const target = e.target;
+        if (e.target.matches('.tabheader__items .tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+});
