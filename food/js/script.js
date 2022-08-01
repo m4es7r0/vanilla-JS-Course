18267279c8aa0d3cbed6f804df9c1316e4776230
m4e7r0 <dodogof02@gmail.com>
2022-08-01 13:11:27 +0300

    add card

--- food/js/script.js
+++ food/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
     // Tabs
 
@@ -103,13 +103,10 @@ window.addEventListener('DOMContentLoaded', () => {
         modalClose = document.querySelector('[data-closeModal]'),
         modalOpen = document.querySelectorAll('[data-modal]');
 
-    const modalTimerID = setTimeout(openModal, 1000);
-
     function openModal() {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        clearTimeout(modalTimerID);
     }
 
     function showModalByScroll() {
@@ -134,5 +131,88 @@ window.addEventListener('DOMContentLoaded', () => {
         if (e.code === "Escape" && modal.classList.contains('show')) closeModal()
     });
     window.addEventListener('scroll', showModalByScroll)
+
+    // Card
+
+    class Card {
+        constructor(parentSelector, img, alt, title, text, price, currency) {
+            this.img = img
+            this.alt = alt
+            this.title = title
+            this.text = text
+            this.price = price
+            this.currency = currency
+            this.parentSelector = document.querySelector(parentSelector);
+        }
+        mount() {
+            const item = document.createElement('div');
+
+            switch (this.currency) {
+                case 'UAH':
+                    this.currency = '₴'
+                    break
+                case 'USD':
+                    this.currency = '$'
+                    break
+                case 'EUR':
+                    this.currency = '€'
+                    break
+                default:
+                    this.currency = '₴'
+            }
+
+            item.innerHTML = `<div class="menu__item">
+                                <img src=${this.img} alt="${this.alt}">
+                                <h3 class="menu__item-subtitle">${this.title}</h3>
+                                <div class="menu__item-descr">${this.text}</div>
+                                <div class="menu__item-divider"></div>
+                                <div class="menu__item-price-wrapper">
+                                    <div class="menu__item-price">
+                                        <div class="menu__item-cost">Цена:</div>
+                                        <div class="menu__item-total"><span>${this.price}</span> <span>${this.currency}</span>/день</div>
+                                    </div>
+                                </div>
+                                </div>
+                             </div>`;
+
+            this.parentSelector.append(item);
+
+        }
+    }
+
+    new Card(
+        '.menu__field .container',
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
+        овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
+        ценой и высоким качеством!`,
+        229,
+        'USD').mount()
+    new Card(
+        '.menu__field .container',
+        'img/tabs/elite.jpg',
+        'elite',
+        'Меню “Премиум”',
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но
+        и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
+        в ресторан!`,
+        550,
+        'EUR').mount()
+    new Card(
+        '.menu__field .container',
+        'img/tabs/post.jpg',
+        'post',
+        'Меню "Постное"',
+        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие
+        продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
+        количество белков за счет тофу и импортных вегетарианских стейков.`,
+        430,
+        'UAH').mount()
+
+    // [cardFirst, cardSecond, cardThid].forEach(i => i.mount())
+
+    
 
 });
